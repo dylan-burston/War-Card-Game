dabc3bf4b279a65a066a32f9cc2a401e4fb209a1
Dylan <dburston0@gmail.com>
2021-11-05 17:11:55 -0400

    redoing project, html, css, js beginning

--- main.js
+++ main.js
@@ -1,31 +1,58 @@
-// CONSTANTS
-
 const suits = ['h', 's', 'd', 'c'];
 const values = ['A', '02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K'];
 
+
+
+
+
+cpuCard = document.getElementById("inPlayCpu");
+playerCard = document.getElementById("inPlayPlayer");
+playerContainerEl = document.querySelector(".playerContainer");
+cpuContainerEl = document.querySelector(".cpuContainer");
+
+
+
+playerContainerEl.addEventListener("click", flipCard);
+
+
+function flipCard() {
+    let randomPCard = suits[Math.floor(Math.random() * suits.length)] + values[Math.floor(Math.random()* values.length)]
+    let randomCpuCard = suits[Math.floor(Math.random() * suits.length)] + values[Math.floor(Math.random()* values.length)]
+    if(randomPCard !== randomCpuCard){
+        playerCard.className = ``
+    }
+
+    }
+    playerCard.className = `card ${randomPCard}`;
+}
+
+
+
+
+// CONSTANTS
+
+// const suits = ['h', 's', 'd', 'c'];
+// const values = ['A', '02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K'];
+
 // VARIABLES
 
-let dealtCards;
-let deckCount = 52;
-let playerCard; // current card
-let compCard; // current card
-let cardCountC; // TOTAL amount of cards computer has
-let cardCountP; // TOTAL amount of cards player has 
+// let dealtCards;
+// let deckCount = 52;
+// let playerCard; // current card
+// let compCard; // current card
+// let cardCountC; // TOTAL amount of cards computer has
+// let cardCountP; // TOTAL amount of cards player has 
+// let initPlay = true; // need to check whether or not its first game, deckCount at 52, no need to check win/ loss
 
-let wonCards // need this variable in order to count the winnings AFTER each turn??
+// let wonCards // need this variable in order to count the winnings AFTER each turn??
 
 // EVENT LISTENERS
 
-// let class1 = "card back-red";
-playerCard = document.getElementById('playerCard') 
-//playerCard.setAttribute("class", class1) // now instead of class1 need to add (suit + value) and pass it through 
 
+// flippedCard()
 
-
-flippedCard()
-
-var playBtn = document.querySelector('#startBtn')
-playBtn.addEventListener('click', play); 
+// var playBtn = document.querySelector('#startBtn')
+// playBtn.addEventListener('click', play); 
 
 
 // classes --> when will classes be used? classes vs what? 
@@ -33,11 +60,13 @@ playBtn.addEventListener('click', play);
 
 // FUNCTIONS 
 
-function play(){
-    console.log('LETS WAAAAR')
-}
+// function play(){
+//     if(initPlay){
+//         initPlay = false;
+//     }
+// }
 // called at the end of EVERY turn IF there is no war so playerCard !== compCard
-function clearBoard(){
+// function clearBoard(){
 
     // in the beginning of each turn... can this be the startup of the game as well?
 
@@ -47,33 +76,30 @@ function clearBoard(){
     // let cardCountC = 0; 
     // let cardCountC = 0;
 
-}
+// }
 
-function flippedCard(value){
-    playerCard.classList.add('card', 'back-red');
-}
+// function flippedCard(value){
+//     playerCard.classList.add('card', 'back-red');
+// }
 
-function showCardValue(value){
-    playerCard.classList.remove('back-red')
-    playerCard.classList.add('card', value);
-}
-
-
-function shuffleDeck(){
-    let deckCount = Math.floor(Math.random() * 52) 
-}
-
-// this function is called at the end of EVERY turn 
-function checkDeck(){
-
-}
+// function showCardValue(value){
+//     playerCard.classList.remove('back-red')
+//     playerCard.classList.add('card', value);
+// }
 
 
-// at the beginning once? or after each turn? because cards dont need to be shuffled after each turh... 
-function remnderGame(){
+// function shuffleDeck(value){
+//     let deckCount = Math.floor(Math.random() * 52) 
+//     let suit1 = Math.ceil(Math.random() * 4) // choose a random number between 1 and 4 inclusive 
+// }
 
-}
+// // this function is called at the end of EVERY turn 
+// function checkDeck(){
+
+// }
 
 
-// choose a random number between 1 and 4 inclusive 
-let suit1 = Math.ceil(Math.random() * 4) 
+// // at the beginning once? or after each turn? because cards dont need to be shuffled after each turh... 
+// function remnderGame(){
+
+// }
