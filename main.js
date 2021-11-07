2f183744ada03e4ed2e0fc028dbbeca829af7935
Dylan <dburston0@gmail.com>
2021-11-07 15:37:16 -0500

    created deck of 52 cards, shuffle function, split half deck to each player

--- main.js
+++ main.js
@@ -9,11 +9,31 @@ cpuCard = document.getElementById("inPlayCpu");
 playerCard = document.getElementById("inPlayPlayer");
 playerContainerEl = document.querySelector(".playerContainer");
 cpuContainerEl = document.querySelector(".cpuContainer");
-
+displayGamePlay = document.querySelector(".instructions"); //display once button pushed 
 
 
 playerContainerEl.addEventListener("click", flipCard);
 
+
+let cards = [];
+suits.forEach(suit => values.forEach(value => cards.push(suit+value)));
+
+function shuffle() {
+    if (cards.length == 52) {
+        cards = cards.sort(() => 0.5 - Math.random());
+    }
+    playerCards = cards.slice(0, cards.length/2);
+    cpuCards = cards.slice(cards.length/2, cards.length);
+    // console.log("Player cards inside shuffle are", playerCards);
+    // console.log("CPU cards inside shuffle are", cpuCards);
+    return [playerCards, cpuCards];
+}
+
+// question: all examples online use classes? why? when is the time to use them?
+
+// function displayGamePlay(){ 
+
+// }
 
 function flipCard() {
     let randomPCard = suits[Math.floor(Math.random() * suits.length)] + values[Math.floor(Math.random()* values.length)]
@@ -40,4 +60,3 @@ function deckWar(){
         // deal 3 more cards // 
     }
 }
-
