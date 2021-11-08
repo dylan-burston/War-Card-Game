2ec54ebbd9a9effae13cc75af31995e6ceb32d0e
Dylan <dburston0@gmail.com>
2021-11-08 16:09:31 -0500

    instructions show first and hit render game to display game

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ const suits = ['h', 's', 'd', 'c'];
 const values = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
 
 
-
-
 cpuCard = document.getElementById("inPlayCpu");
 playerCard = document.getElementById("inPlayPlayer");
 playerContainerEl = document.querySelector(".playerContainer");
 cpuContainerEl = document.querySelector(".cpuContainer");
 displayGamePlay = document.querySelector(".instructions"); //display once button pushed 
+playerTallyEl = document.querySelector(".playerTally");
+cpuTallyEl = document.querySelector(".cpuTally");
+shuffleBtnEl = document.querySelector(".shuffle");
+gameBodyEl = document.querySelector('.gameBody');
+instructionsEl = document.querySelector('.instructions');
 
-
+shuffleBtnEl = document.addEventListener("click", shuffle);
 playerContainerEl.addEventListener("click", flipCard);
 
 
@@ -18,40 +21,46 @@ let cards = [];
 suits.forEach(suit => values.forEach(value => cards.push(suit+value)));
 
 function shuffle() {
-    if (cards.length == 52) {
+    gameBodyEl.style = "display: block";
+    instructionsEl.style = "display: none";
+    if (cards.length === 52) {
         cards = cards.sort(() => 0.5 - Math.random());
     }
     playerCards = cards.slice(0, cards.length/2);
     cpuCards = cards.slice(cards.length/2, cards.length);
-    // console.log("Player cards inside shuffle are", playerCards);
-    // console.log("CPU cards inside shuffle are", cpuCards);
+    console.log("Player cards inside shuffle are", playerCards);
+    console.log("CPU cards inside shuffle are", cpuCards);
     return [playerCards, cpuCards];
 }
 
-// question: all examples online use classes? why? when is the time to use them?
-
-// function displayGamePlay(){ 
-
-// }
-
 function flipCard() {
-    const [playerCards, cpuCards] = shuffle();
     // console.log("player cards", playerCards);
     // console.log("cpu cards", cpuCards);
     let randomPCard = playerCards[Math.floor(Math.random()*playerCards.length)]
     let randomCpuCard = cpuCards[Math.floor(Math.random()*cpuCards.length)]
+    compare(randomPCard, randomCpuCard, playerCards, cpuCards) // why?
+    playerCard.className = `card large ${randomPCard}`;
+    cpuCard.className = `card large ${randomCpuCard}`;
+    }
 
-    
-    // if(randomPCard !== randomCpuCard){
-    //     playerCard.className = `card large ${randomPCard}`;
-    //     cpuCard.className = `card large ${randomCpuCard}`;
-    // }
-}
 
-function compare(randomPCard, randomCpuCard, playerCards, cpuCards){
-     // want to compare and snatch other cards if i win
-     let numericalPlayer;
-     let numericalCpu;
+function compare(randomPCard, randomCpuCard, playerCards, cpuCards) {
+    let numericalPlayer;
+    let numericalCpu;
+    numericalPlayer = determineValue(randomPCard);
+    numericalCpu = determineValue(randomCpuCard);
+    // console.log("random cards and their values", randomPCard, randomCpuCard,  numericalPlayer, numericalCpu);
+    if (numericalPlayer > numericalCpu) {
+        playerCards.push(randomCpuCard);
+        let cpuCardIndex = cpuCards.indexOf(randomCpuCard);
+        cpuCards.splice(cpuCardIndex, 1); // get ONE element out of the array located at cpuCardIndex
+    } else if (numericalCpu > numericalPlayer) {
+        cpuCards.push(randomPCard);
+        let pCardIndex = playerCards.indexOf(randomPCard);
+        playerCards.splice(pCardIndex, 1);
+    } else {
+        console.log('war')
+    }
 }
 
 function determineValue(incomingCard) {
