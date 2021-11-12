f5d1e01a135d60cd0df25dab62d983440363e11c
Dylan <dburston0@gmail.com>
2021-11-12 14:57:35 -0500

    retyped everything, wasnt working properly, restarded vs code and did again

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ cpuCard = document.getElementById("inPlayCpu");
 playerCard = document.getElementById("inPlayPlayer");
 playerContainerEl = document.querySelector(".playerContainer");
 cpuContainerEl = document.querySelector(".cpuContainer");
-displayGamePlay = document.querySelector(".instructions"); 
+displayGamePlay = document.querySelector(".instructions");
 playerTallyEl = document.querySelector(".playerTally");
 cpuTallyEl = document.querySelector(".cpuTally");
 shuffleBtnEl = document.querySelector(".shuffle");
@@ -17,8 +17,9 @@ forceWinBtnEl = document.querySelector('.forceWin')
 endGameEl = document.querySelector('.endGame')
 announcementEl = document.querySelector(".announcement");
 warPCardEl = document.getElementById("warPCard");
-warCpuCardEl = document.getElementById("warCpuCard")
-
+warCpuCardEl = document.getElementById("warCpuCard");
+warBackRedEl = document.getElementById("warBackRed");
+warBackBlueEl = document.getElementById("warBackBlue");
 
 shuffleBtnEl.addEventListener("click", shuffle);
 playerContainerEl.addEventListener("click", flipCard);
@@ -33,14 +34,16 @@ let lastFourCpuCards;
 let lastFourPlayerCards;
 
 function shuffle() {
-    warPCardEl.className = "";  
+    warPCardEl.className = "";
     warCpuCardEl.className = "";
-    playerCard.className = "card large joker-black"; 
+    warBackBlueEl.className = "";
+    warBackRedEl.className = "";
+    playerCard.className = "card large joker-black";
     cpuCard.className = "card large joker-red";
     gameBodyEl.style = "display: block";
     instructionsEl.style = "display: none";
     announcementEl.style = "display: none";
-    cards = cards.sort(() => 0.5 - Math.random()); 
+    cards = cards.sort(() => 0.5 - Math.random());
     playerCards = cards.slice(0, cards.length/2);
     cpuCards = cards.slice(cards.length/2, cards.length);
     playerTallyEl.innerText = ` ${playerCards.length}`;
@@ -48,34 +51,35 @@ function shuffle() {
 }
 
 function flipCard() {
-    warPCardEl.className = "";  
+    warPCardEl.className = "";
     warCpuCardEl.className = "";
-    let randomPCard = playerCards[Math.floor(Math.random()*playerCards.length)] 
+    warBackBlueEl.className = "";
+    warBackRedEl.className = "";
+    let randomPCard = playerCards[Math.floor(Math.random()*playerCards.length)]
     let randomCpuCard = cpuCards[Math.floor(Math.random()*cpuCards.length)]
     playerCard.className = `card large ${randomPCard}`;
     cpuCard.className = `card large ${randomCpuCard}`;
-    [playerCards, cpuCards] = compare(randomPCard, randomCpuCard, playerCards, cpuCards); // cards yuou are comparing, someone wins someone takes, arrays WILL be modified 
+    [playerCards, cpuCards] = compare(randomPCard, randomCpuCard, playerCards, cpuCards); // cards yuou are comparing, someone wins someone takes, arrays WILL be modified
     tally(playerCards, cpuCards)
 }
 
 function compare(randomPCard, randomCpuCard, playerCards, cpuCards) {
-    let numericalPlayer = determineValue(randomPCard); 
+    let numericalPlayer = determineValue(randomPCard);
     let numericalCpu = determineValue(randomCpuCard);
     if (numericalPlayer > numericalCpu) {
         playerCards.push(randomCpuCard);
         let cpuCardIndex = cpuCards.indexOf(randomCpuCard);
-        cpuCards.splice(cpuCardIndex, 1); 
+        cpuCards.splice(cpuCardIndex, 1);
     } else if (numericalCpu > numericalPlayer) {
         cpuCards.push(randomPCard);
         let pCardIndex = playerCards.indexOf(randomPCard);
         playerCards.splice(pCardIndex, 1);
-    } else if (numericalCpu === numericalPlayer && playerCards.length >= 4 && cpuCards.length >= 4){ 
-        [playerCards, cpuCards] = war(playerCards, cpuCards) // resetting reasigning cpu, playercards 
+    } else if (numericalCpu === numericalPlayer && playerCards.length >= 4 && cpuCards.length >= 4){
+        [playerCards, cpuCards] = war(playerCards, cpuCards) 
     } else {
-        displayWinner(playerCards, cpuCards); 
+        displayWinner(playerCards, cpuCards);
     }
     return [playerCards, cpuCards]
-       
     }
 
 function determineValue(incomingCard) {
@@ -83,7 +87,7 @@ function determineValue(incomingCard) {
     let trueValues = [2,3,4,5,6,7,8,9,10,11,12,13,14];
     for (let i=0; i<values.length; i++) {
         if (incomingCard.substring(1) === values[i]) {
-            numericalValue = trueValues[i]; 
+            numericalValue = trueValues[i];
         }
     }
     return numericalValue;
@@ -92,25 +96,30 @@ function determineValue(incomingCard) {
 function war(playerCards, cpuCards){
     lastFourPlayerCards = playerCards.slice(Math.max(playerCards.length -4, 0));
     lastFourCpuCards = cpuCards.slice(Math.max(cpuCards.length -4, 0));
-    // let warPCard = lastFourPlayerCards[lastFourPlayerCards.length - 1]; // last one of each 
+    console.log("Player cards before war:", playerCards, "CPU cards before war:", cpuCards);
+    // let warPCard = lastFourPlayerCards[lastFourPlayerCards.length - 1]; // last one of each
     // let warCpuCard = lastFourCpuCards[lastFourCpuCards.length - 1];
-    let warPCard = lastFourPlayerCards[Math.floor(Math.random()*lastFourPlayerCards.length)] 
-    let warCpuCard = lastFourCpuCards[Math.floor(Math.random()*lastFourCpuCards.length)] 
+    let warPCard = lastFourPlayerCards[Math.floor(Math.random()*lastFourPlayerCards.length)]
+    let warCpuCard = lastFourCpuCards[Math.floor(Math.random()*lastFourCpuCards.length)]
     warPValue = determineValue(warPCard);
     warCpuValue = determineValue(warCpuCard);
     if (warCpuValue === warPValue) {
         [playerCards, cpuCards] = war(playerCards, cpuCards);
     }
+    warBackBlueEl.className = "card small back-blue";
+    warBackRedEl.className = "card small back-red";
     warPCardEl.className = `card large ${warPCard}`;
     warCpuCardEl.className = `card large ${warCpuCard}`;
     let [lastPlayerCards, lastCpuCards] = compare(warPCard, warCpuCard, lastFourPlayerCards, lastFourCpuCards);
     if (lastPlayerCards.length > lastCpuCards.length){
-        playerCards = [playerCards, warCpuCard, lastCpuCards].flat(); 
+        playerCards = [playerCards, warCpuCard, lastCpuCards].flat();
         cpuCards = cpuCards.slice(0,cpuCards.length-4);
+        console.log("Player cards after war:", playerCards, "CPU cards after war:", cpuCards);
     } else {
-        cpuCards = [cpuCards, lastPlayerCards].flat();
+        cpuCards = [cpuCards, warPCard, lastPlayerCards].flat();
         playerCards = playerCards.slice(0,playerCards.length-4);
-    }   
+        console.log("Player cards after war:", playerCards, "CPU cards after war:", cpuCards);
+    }
     return [playerCards, cpuCards]
 }
 
@@ -135,11 +144,9 @@ function forceWin(playerCards, cpuCards){
    displayWinner(playerCards, cpuCards)
 }
 
-// THE BELOW FUNCTION CAN BE UNCOMMENTED OUT IN ORDER TO AUTOMATICALLY RUN THROUGH A FULL GAME TO SEE THE WIN/LOSS CONDITION QUICKER 
-
+// THE BELOW FUNCTION CAN BE UNCOMMENTED OUT IN ORDER TO AUTOMATICALLY RUN THROUGH A FULL GAME TO SEE THE WIN/LOSS CONDITION QUICKER
 // function simulateGame() {
-//     shuffle(); 
+//     shuffle();
 //     setInterval(() => flipCard(playerCard, cpuCard), 100);
 // }
 // setTimeout(simulateGame, 1000);
-
